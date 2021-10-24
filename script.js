34084e7dee6929b5aa098c00df53696b0fa35a0a
slobeann <saloneeg@andrew.cmu.edu>
2021-10-24 10:28:41 -0400

    added cart functionality to other pages

--- script.js
+++ script.js
@@ -10,17 +10,29 @@ function ready(){
 var overlay = document.getElementById("overlay");
 var checkout = document.querySelector(".checkout");
 
-document.querySelector(".add-to-cart").addEventListener("click", function(){
-  overlay.style.display="block";
-  checkout.classList.add("slide-in");
+document.querySelector(".navigation .cart-button").addEventListener("click", function(event){
+  console.log('hi')
+  event.preventDefault()
+  openCheckOut()
 })
 
+var addToCart= document.querySelector(".add-to-cart")
+if (addToCart){
+  addToCart.addEventListener('click', function(){
+    overlay.style.display="block";
+    checkout.classList.add("slide-in");
+  })
+}
 
 document.querySelector("#overlay-close").addEventListener("click", function(){
   overlay.style.display="none";
   checkout.classList.remove("slide-in");
 })
 
+function openCheckOut(){
+  overlay.style.display="block";
+  checkout.classList.add("slide-in");
+}
 
 // for shopping cart functionality
 var removeCartItemButtons = document.getElementsByClassName("trash")
