41b782430cc76ce38df48fa157eba09f53c813c6
slobeann <saloneeg@andrew.cmu.edu>
2021-11-07 07:42:41 -0500

    cart updates

--- script.js
+++ script.js
@@ -74,6 +74,8 @@ function updateCartTotal(){
   document.getElementsByClassName("cart-total")[0].innerText = "$" + total
   document.getElementsByClassName("bill-total")[0].innerText = "$" + billTotal
   document.getElementsByClassName("cart-heading")[0].innerText= "cart(" + totalamount +")"
+  document.getElementsByClassName("cart-button")[0].innerText = "cart(" + totalamount +")"
+
 }
 
 function addToCartClick(event){
